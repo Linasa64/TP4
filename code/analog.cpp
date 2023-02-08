3eeb9a7f085cf2cab46310c1d685720ce7fe3efc
Lea-drd <73934530+Lea-drd@users.noreply.github.com>
2023-02-08 10:05:55 +0100

    recuperation des options

--- code/analog.cpp
+++ code/analog.cpp
@@ -11,21 +11,23 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   /*
-         // options -g -e -t heure
-      int arg [4] = {0, 0, 0, 0};
-      int i = 1;
-      for (i, i<argc, i++){
-         if(strcmp(argv[i], "-g")
-            arg[0] = 1;
-         else if(strcmp(argv[i], "-e")
-            arg[1] = 1;
-         else if(strcmp(argv[i], "-t")
-            arg[2] = 1;
-            arg[3] = argv[i+1];
-         } 
-      }
-   */
+		// options -g -e -t heure
+	int arg [3] = {0, 0, 0};
+	string heure = "";
+	int i = 1;
+	
+	for (i = 1; i<argc; i++){
+		if(!strcmp(argv[i], "-g"))
+			arg[0] = 1;
+		else if(!strcmp(argv[i], "-e"))
+			arg[1] = 1;
+		else if(!strcmp(argv[i], "-t"))
+		{
+			arg[2] = 1;
+			heure = argv[i+1];
+			cout << heure << endl;
+		} 
+   }
    Historique * h = new Historique();
    h->Top10();
    map<string, pair<int, map<string, int>>> m = h->GetMapComplete();
